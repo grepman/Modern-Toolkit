fd6ae2ae69b2aeceb2df26552f5245acda629e10
Shashikant86 <shashikant.jagtap@aol.co.uk>
2014-03-30 12:58:07 +0100

    Added New config

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInitfbd57ca6db7f6353f9310b082ff33f0e::getLoader();
+return ComposerAutoloaderInit2d81742b1e230d038315725f680ddd41::getLoader();
